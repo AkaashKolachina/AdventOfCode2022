65eaa7a7c329b3ea5c1cc0b22ed504f2a1e07195
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2023-01-07 19:23:35 +0530

    Finally

---
+++ 20.1.py
@@ -0,0 +1,24 @@
+from sys import argv
+from collections import deque
+
+
+data = open(argv[1]).read().strip().split('\n')
+dict = [int(x) for x in data]
+n = len(dict)
+file = [num for num in range(n)]
+
+for i in range(n):
+    dist = dict[i]
+    idx = file.index(i)
+    file.remove(i)
+    new_idx = (idx + dist) % len(file)
+    file.insert(new_idx,i)
+    
+for i in range(n):
+    file[i] = dict[file[i]]
+
+i = file.index(0)
+coords = (file[(i + 1000) % n],file[(i + 2000) % n],file[(i + 3000) % n])
+print(coords)
+print(sum(coords))
+
