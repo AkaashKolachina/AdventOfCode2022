09c16cd42dc5abe7be818f3ccebc2d8a51c2b872
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2023-01-12 00:27:12 +0530

    23 part 2

---
+++ 23.2.py
@@ -0,0 +1,59 @@
+from sys import argv
+from collections import defaultdict
+
+elves = set()
+data = open(argv[1]).read().strip().split('\n')
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if data[i][j] == '#':
+            elves.add((i,j))
+
+dirs = [(-1,0), (1,0), (0,-1), (0,1)]
+all_dirs = {}
+all_dirs[(0,-1)] = [(0,-1), (1,-1), (-1,-1)]
+all_dirs[(0,1)] = [(0,1), (1,1), (-1,1)]
+all_dirs[(-1,0)] = [(-1,0), (-1,1), (-1,-1)]
+all_dirs[(1,0)] = [(1,0), (1,-1), (1,1)]
+num_rounds = 0
+done = False
+while not done:
+    done = True
+    moves = defaultdict(list)
+    # Stage 1
+    for elf in elves:
+        (x,y) = elf
+        coords = [-1,0,1]
+        all_clear = True
+        for i in coords:
+            for j in coords:
+                if (i,j) != (0,0) and (x + i, y + j) in elves:
+                    all_clear = False
+                    done = False
+                    break
+        if all_clear:
+            continue
+
+        proposed_move = False
+        for dir in dirs:
+            if proposed_move:
+                break
+            for (dx,dy) in all_dirs[dir]:
+                has_elf = False
+                if (x + dx, y + dy) in elves:
+                    has_elf = True
+                    break
+            if not has_elf:
+                moves[(x + dir[0], y + dir[1])].append(elf)
+                proposed_move = True
+    # Stage 2
+    for dest,origins in moves.items():
+        if len(origins) == 1:
+            elves.remove(origins[0])
+            elves.add(dest)
+
+    dir = dirs[0]
+    dirs.remove(dir)
+    dirs.append(dir)
+    num_rounds += 1
+
+print(num_rounds)
